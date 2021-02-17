c8fbef00244eda81a6a94323f274e16793c6cb18
Cedric Lima <limacedric@hotmail.fr>
2021-02-17 21:36:03 +0100

    Ajout page about

---
+++ src/components/Pages/About.vue
@@ -0,0 +1,11 @@
+<template>
+   <div>
+      <h1>A Propos</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aspernatur earum sint iste tenetur officiis quia praesentium iusto corporis voluptas.</p>
+   </div>
+</template>
+<script>
+export default {
+   
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+
+import Home from '@/components/Pages/Home';
+import About from '@/components/Pages/About';
+
+Vue.use(VueRouter)
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+
+const routes = [
+   {path:'/', component:Home},
+   {path:'/about', component:About}
+]
+const router = new VueRouter({
+   routes
+})
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
